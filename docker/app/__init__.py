03d29990848fd75b293172e2707e8a1bc9740c14
skabrits <seva.kabrits@gmail.com>
2024-06-13 09:28:17 +0300

    fixed bugs

--- docker/app/__init__.py
+++ docker/app/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, g
+from flask import Flask
 from flask_cors import CORS
 import logging
 import sys
+import cv2
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ CORS(app)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.DEBUG)
 
-g.last_frame = None
-g.vid = cv2.VideoCapture(0)
+app.config['camera'] = cv2.VideoCapture(0)
+app.config['last_frame'] = None
 
 from app import views
--- docker/app/views.py
+++ docker/app/views.py
@@ -1,19 +1,20 @@
 from flask import Flask
-from app import app, g
+from app import app
 import json
 import imutils
 import base64
+import cv2
 
 @app.route("/")
 def index():
-    if g.vid.isOpened():
-        ret, frame = g.vid.read()
+    if app.config['camera'].isOpened():
+        ret, frame = app.config['camera'].read()
         if ret:
             frame = imutils.resize(frame, width=400)
             image_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
-            g.last_frame = image_bytes
+            app.config['last_frame'] = image_bytes
         else:
-            image_bytes = g.last_frame
+            image_bytes = app.config['last_frame']
     else:
         image_bytes = last_frame
     image_base64 = base64.b64encode(image_bytes)
